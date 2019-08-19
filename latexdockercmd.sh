192f85f49d2a2d47eb093b61124a5f808a5c8c4d
thomas <thomas@adress>
2019-08-19 02:08:20 +0200

    clean up

--- latexdockercmd.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo "usage: $0 pdflatex -output-directory data/presentation/ beamer_pres.tex"
-IMAGE=latex-docker/base:ubuntu
-exec docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "$PWD":/data "$IMAGE" "pdflatex" --output-directory /data /data/autoencoder.tex
-
